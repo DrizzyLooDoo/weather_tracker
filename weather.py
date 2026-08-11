39f375bc0a5de0ece0e1ab1d14e51746ee927d1e
Arjun Althuru <arjunalthuru@icloud.com>
2026-08-11 12:45:38 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -105,10 +105,11 @@ current_time = current_data['current']['time']
 temp_c = current_temp
 temp_f = round((temp_c) * 9/5 + 32, 1)
 log_file = 'daily_log.csv'
-log_df = pd.DataFrame({'date': [str(today)],
-'time': [current_time],
-'temperature_2m': [current_temp],
-'temp_f': [temp_f]
+log_df = pd.DataFrame({
+    'date': [str(today)],
+    'time': [current_time],
+    'temperature_2m': [current_temp],
+    'temp_f': [temp_f]
 })
 log_df.to_csv(log_file, mode='a', header=not os.path.isfile(log_file), index=False)
 print(f"Logged current temperature: {current_temp} degrees C at {current_time}")
